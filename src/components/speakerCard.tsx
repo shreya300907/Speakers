c7e7a5d020c980a68986f628fa2068f6a6ba046e
shreya300907 <kumarishreya300907@gmail.com>
2026-05-26 15:07:27 +0530

    "making it responsive"

--- src/components/speakerCard.tsx
+++ src/components/speakerCard.tsx
@@ -1,28 +1,48 @@
+"use client";
+import { useState } from "react";
+
 type SpeakerCardProps = {
     speaker: string;
     title: string;
 };
 
 export default function SpeakerCard({ speaker, title }: SpeakerCardProps) {
+    const [isActive, setIsActive] = useState(false);
+
     return (
-        <div className="group relative overflow-hidden border-2 border-white rounded-xl hover:transition-transform duration-500 hover:-translate-y-3 hover:shadow-md hover:shadow-white">
+        <div
+            onClick={() => setIsActive(!isActive)}
+            className={`group relative overflow-hidden border-2 border-white rounded-xl transition-all duration-500 cursor-pointer
+                hover:-translate-y-3 hover:shadow-md hover:shadow-white
+                ${isActive ? "-translate-y-3 shadow-md shadow-white" : ""}`}
+        >
             <div>
                 <img
                     src={`/${speaker}.png`}
                     alt={`${speaker}`}
-                    className="h-80 object-cover rounded-t-xl transition-all duration-300 group-hover:blur-sm group-hover:brightness-50"
+                    className={`h-80 min-w-60 object-cover rounded-t-xl transition-all duration-300
+                        group-hover:blur-sm group-hover:brightness-50
+                        ${isActive ? "blur-sm brightness-50" : ""}`}
                 />
-                <div className="transition-opacity ease-out duration-500 absolute bottom-5 pl-4 flex flex-col gap-3 opacity-0 group-hover:opacity-100">
+                <div className={`transition-opacity ease-out duration-500 absolute bottom-5 pl-4 flex flex-col gap-3
+                    opacity-0 group-hover:opacity-100
+                    ${isActive ? "opacity-100" : ""}`}
+                >
                     <h2 className="text-xl font-semibold text-left text-white">{`${speaker}`}</h2>
                     <div className="text-zinc-300 text-xs ">
                         {`${title}`}
                     </div>
-                    <button className="bg-red-600 text-white font-semibold text-sm py-1 text-center rounded-md cursor-pointer">
+                    <button className="bg-red-600 text-white font-semibold text-sm py-1 rounded-md cursor-pointer">
                         Know more
                     </button>
                 </div>
             </div>
-            <h2 className="text-xl font-semibold text-left pl-4 py-2 text-red-500 group-hover:text-black">{`${speaker}`}</h2>
+            <h2 className={`text-xl font-semibold text-left pl-4 py-2 text-red-500 tracking-[0.3rem] transition-colors duration-300
+                group-hover:text-black
+                ${isActive ? "text-black" : ""}`}
+            >
+                {`${speaker}`}
+            </h2>
         </div>
     );
 }
