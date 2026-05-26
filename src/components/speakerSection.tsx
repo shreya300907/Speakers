3e338aec02c0e0fc31952c4dc35121ecc7b8e27f
shreya300907 <kumarishreya300907@gmail.com>
2026-05-26 17:26:37 +0530

    "small fix"

--- src/components/speakerSection.tsx
+++ src/components/speakerSection.tsx
@@ -5,14 +5,17 @@ export default function SpeakerSection({ speakers, isLoading }: { speakers: Spea
     return (
         <>
             {isLoading ? (
-                Array.from({ length: 8 }).map((_, index) => (
-                    <SpeakerCard
-                        key={index}
-                        speaker=""
-                        title=""
-                        isLoading={true}
-                    />
-                ))
+                <div className="px-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-16 gap-y-8 place-items-center ">
+                    {Array.from({ length: 8 }).map((_, index) => (
+                        <SpeakerCard
+                            key={index}
+                            speaker=""
+                            title=""
+                            isLoading={true}
+                        />
+                    ))
+                    }
+                </div>
             ) : (
                 <div className="px-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-16 gap-y-8 place-items-center ">
                     {speakers.map((speaker) => (
